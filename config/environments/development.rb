0752d32c6a97bee5f0d17f3c03f52c92feee2454
Ubuntu <ubuntu@ip-172-31-95-152.ec2.internal>
2019-12-19 19:42:14 +0000

    add email functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = {:host => 'https://94963ae8804a46548da5e3cc2d7cc37b.vfs.cloud9.us-east-1.amazonaws.com/'} 
 
   # Show full error reports.
   config.consider_all_requests_local = true
