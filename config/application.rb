465f7ef4daf8c961eab1e992154570260afdf431
Ubuntu <ubuntu@ip-172-31-95-152.ec2.internal>
2019-12-28 15:53:17 +0000

    change config

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,11 @@ Bundler.require(*Rails.groups)
 
 module SaasApp
   class Application < Rails::Application
-    
+    config.middleware.use Apartment::Elevators::Subdomain
+ 
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-      config.middleware.use Apartment::Elevators::Subdomain
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
