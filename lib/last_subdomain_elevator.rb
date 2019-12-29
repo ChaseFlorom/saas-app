5c0a1ecc0ae769c35e848988367d3807805e24a4
Ubuntu <ubuntu@ip-172-31-95-152.ec2.internal>
2019-12-29 03:34:48 +0000

    testing redirect

--- lib/last_subdomain_elevator.rb
+++ lib/last_subdomain_elevator.rb
@@ -2,9 +2,13 @@ require 'apartment/elevators/subdomain'
 class LastSubdomainElevator < Apartment::Elevators::Subdomain
   protected
   # Only care about the last subdomain for the database name
-  def subdomain(host)
-    subdomains(host).last
-    ## Alternatively, use all segments of the subdomain
-    # subdomains(host).join(".")
-  end
+    def subdomain(host)
+        subdomains(host).last
+        ## Alternatively, use all segments of the subdomain
+        # subdomains(host).join(".")
+    rescue Apartment::TenantNotFound
+        Rails.logger.error "ERROR: Apartment Tenant"
+        Apartment::Tenant.reset
+        redirect_to root_path
+    end
 end
