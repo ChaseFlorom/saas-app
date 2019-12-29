4e32e53ef35d3b2645bb5ed24e9e00a057c9fbc0
Ubuntu <ubuntu@ip-172-31-95-152.ec2.internal>
2019-12-29 03:57:45 +0000

    trying something

--- lib/last_subdomain_elevator.rb
+++ lib/last_subdomain_elevator.rb
@@ -1,5 +1,28 @@
 require 'apartment/elevators/subdomain'
 class LastSubdomainElevator < Apartment::Elevators::Subdomain
+  def self.excluded_subdomains
+        @excluded_subdomains ||= ['www']
+  end
+
+  def self.excluded_subdomains=(arg)
+    @excluded_subdomains = arg
+  end
+
+  def parse_tenant_name(request)
+    request_subdomain = subdomain(request.host)
+
+    # If the domain acquired is set to be excluded, set the tenant to whatever is currently
+    # next in line in the schema search path.
+    tenant = if self.class.excluded_subdomains.include?(request_subdomain)
+      nil
+    else
+      request_subdomain
+    end
+
+    tenant.presence
+  end
+  
+  
   protected
   # Only care about the last subdomain for the database name
     def subdomain(host)
